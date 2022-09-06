b913b27ea44fa9ad16b07f3094b72bc8a6616f76
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-06 16:44:34 +0100

    added more questions and pictures to questions data array and made sure they render in the app

--- utilities/QuestionsData.tsx
+++ utilities/QuestionsData.tsx
@@ -40,6 +40,42 @@ export const questionsData: Array<Questions> = [
       { id: "4", value: "Tiger", label: "Tiger" },
     ],
   },
+  {
+    name: "image3",
+    image: require("../assets/BladeRunner.jpg"),
+    description: "Blade Runner Aesthetics",
+    question: "What city is Blade Runner set in?",
+    answers: [
+      { id: "1", value: "New York", label: "New York" },
+      { id: "2", value: "Los Angeles", label: "Los Angeles" },
+      { id: "3", value: "Chicago", label: "Chicago" },
+      { id: "4", value: "Miami", label: "Miami" },
+    ],
+  },
+  {
+    name: "image4",
+    image: require("../assets/Serenity.jpg"),
+    description: "Serenity Ship",
+    question: "In the TV-show Firefly, what is the Serenity named after?",
+    answers: [
+      { id: "1", value: "State of Mind", label: "State of Mind" },
+      { id: "2", value: "Planet", label: "Planet" },
+      { id: "3", value: "Person", label: "Person" },
+      { id: "4", value: "Battle", label: "Battle" },
+    ],
+  },
+  {
+    name: "image5",
+    image: require("../assets/ETMoon.jpg"),
+    description: " Poster of E.T.",
+    question: "What's the the name of the boy who find and befriends E.T.?",
+    answers: [
+      { id: "1", value: "Lucas", label: "Lucas" },
+      { id: "2", value: "Johnny", label: "Johnny" },
+      { id: "3", value: "Elliot", label: "Elliot" },
+      { id: "4", value: "Dennis", label: "Dennis" },
+    ],
+  },
 ];
 
 export const GetImage = (name: string) => {
