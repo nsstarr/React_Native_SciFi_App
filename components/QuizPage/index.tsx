217cfc0e95ae780eaa7b8b25f119280f2850dd9d
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-08 12:24:33 +0100

    implemented swiper but doesn't work with a header and retake quiz button goes back to show results page

--- components/QuizPage/index.tsx
+++ components/QuizPage/index.tsx
@@ -1,80 +1,99 @@
-import React, {useState} from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
-import Header from '../Header'
-import QuizAnswer from '../QuizAnswer'
-import QuizQuestion from '../QuizQuestion'
-import QuizPicture from '../QuizPicture'
-import {questionsData} from "../../utilities/QuestionsData"
-import {GetImage} from "../../utilities/images"
-import RadioGroup, {RadioButtonProps } from 'react-native-radio-buttons-group'
-import type {NativeStackScreenProps} from "@react-navigation/native-stack"
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Header from "../Header";
+import QuizAnswer from "../QuizAnswer";
+import QuizQuestion from "../QuizQuestion";
+import QuizPicture from "../QuizPicture";
+import { questionsData } from "../../utilities/QuestionsData";
+import { GetImage } from "../../utilities/images";
+import RadioGroup, { RadioButtonProps } from "react-native-radio-buttons-group";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Swiper from "react-native-swiper";
 type StackTypes = {
   Home: undefined;
   QuizPage: undefined;
-  ResultsPage: undefined
-}
+  ResultsPage: undefined;
+};
 
-type Props = NativeStackScreenProps<StackTypes, 'QuizPage'>
+type Props = NativeStackScreenProps<StackTypes, "QuizPage">;
 
-const QuizPage = ({navigation}:Props) => {
-    const [questionNumber, setQuestionNumber] = useState(0)
+const QuizPage = ({ navigation }: Props) => {
+  const [questionNumber, setQuestionNumber] = useState(0);
 
-    const [answerTracker, setAnswerTracker] = useState({})
-    let answersData: RadioButtonProps[] = questionsData[questionNumber].answers!
-   
+  const [answerTracker, setAnswerTracker] = useState({});
+  let answersData: RadioButtonProps[] = questionsData[questionNumber].answers!;
 
-    const [answers, setAnswers] = useState<RadioButtonProps[]>(answersData)
+  const [answers, setAnswers] = useState<RadioButtonProps[]>(answersData);
 
-    function changeQuestion(){
-        setQuestionNumber(prev => prev + 1)
-    }
+  function changeQuestion() {
+    setQuestionNumber((prev) => prev + 1);
+  }
 
+  function onPressRadioButton(radioButtonsArray: RadioButtonProps[]) {
+    let answer = radioButtonsArray.filter((object) => {
+      if (object.selected) {
+        return object;
+      }
+    });
+    setAnswerTracker({ ...answerTracker, [questionNumber]: answer[0].value });
+    setAnswers(radioButtonsArray);
+  }
 
+  function checkAnswers() {
+    console.log(questionNumber);
+    console.log(`Length of quiz is ${questionsData.length}`);
+    console.log(answerTracker);
+    console.log(questionsData[questionNumber]);
+  }
 
-    function onPressRadioButton(radioButtonsArray:RadioButtonProps[]) {
-        let answer = radioButtonsArray.filter((object)=>{if(object.selected){return object}})
-        setAnswerTracker({...answerTracker, [questionNumber]:answer[0].value})
-        setAnswers(radioButtonsArray);
-    }
+  const yoda = GetImage(`image${questionNumber}`, questionsData)!;
 
-    function checkAnswers() {
-        console.log(questionNumber)
-        console.log(`Length of quiz is ${questionsData.length}`)
-        console.log(answerTracker)
-        console.log(questionsData[questionNumber])
-    }
+  if (questionNumber >= questionsData.length - 1) {
+    return (
+      <View>
+        <Text>You finished the quiz, press the button to see your results</Text>
+        <Button
+          title="To Results"
+          onPress={() => {
+            navigation.navigate("ResultsPage");
+          }}
+        ></Button>
+      </View>
+    );
+  } else {
+    return (
+    //   <View>
+    //     <Header />
+        <Swiper>
 
-    const yoda = GetImage(`image${questionNumber}`, questionsData)!
-
-    if(questionNumber >= (questionsData.length -1 )){
-        return(
-            <View>
-                <Text>You finished the quiz, press the button to see your results</Text>
-                <Button title="To Results" onPress={() => {navigation.navigate('ResultsPage')}}></Button>
-            </View>
-        )
-    }
-    else {
-        return (
-        <View>
-            <Header/>
-            <View style={styles.pictureContainer}>
-                <QuizPicture source={ yoda}/>
-                <QuizQuestion question={questionsData[(questionNumber)].question!} />
-                <RadioGroup radioButtons={questionsData[(questionNumber)].answers!} onPress={onPressRadioButton}/>
+          {questionsData.map((question, key) => {
+            
+            return (
+              <View style={styles.pictureContainer} key = {key}>
+                <QuizPicture source={question.image} />
+                <QuizQuestion
+                  question={question.question!}
+                />
+                <RadioGroup
+                  radioButtons={question.answers!}
+                  onPress={onPressRadioButton}
+                />
                 <Button onPress={changeQuestion} title="Next"></Button>
                 <Button onPress={checkAnswers} title="Check"></Button>
-            </View>
-        </View>
-    )
-    }
-}
+              </View>
+            );
+          })}
+        </Swiper>
+    //   </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-    pictureContainer:{
-        alignItems: 'center',
-        margin: 50
-    }
-})
+  pictureContainer: {
+    alignItems: "center",
+    margin: 50,
+  },
+});
 
-export default QuizPage
+export default QuizPage;
