3ccc5cbac2c27b6771ecc7f42b3ff4dfaab2057c
St0neofFr33dom <christophefreeston@googlemail.com>
2022-09-13 12:07:49 +0100

    Trying to add shadows using the built in shadow props, doesn't appear on android, test on ios to see if it works there

--- components/HomeScreen/index.tsx
+++ components/HomeScreen/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, StyleSheet} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import Header from "../Header";
 import QuizCategoryCard from "../QuizCategoryCard";
 import {images, GetImage} from "../../utilities/images"
@@ -22,14 +23,36 @@ const HomeScreen = ({navigation}:Props) => {
   const avatar = GetImage(`image${0}`,images)
   console.log(avatar);
   return (
+    <LinearGradient colors={["rgba(75,0,130,1)","rgba(75,0,130,0.64)","rgba(75,0,130,0.64)","rgba(75,0,130,1)"]} style={{height:"100%"}}>
+        <View style={style.whiteRectangle}></View>
     <View>
       <Header avatar={avatar}/>
       <ScrollView>
         {/* Add .map() later on when more quizzed are added */}
         <QuizCategoryCard name="Sci-Fi" navigation = {navigation} />
+        <QuizCategoryCard name="Sci-Fi" navigation = {navigation} />
+        <QuizCategoryCard name="Sci-Fi" navigation = {navigation} />
       </ScrollView>
     </View>
+    </LinearGradient>
   );
 };
+
+const style = StyleSheet.create({
+  // container:{
+  //   backgroundColor:"linear-gradient(rgba(75,0,130,1),rgba(255,255,255,1))",
+  //   height:"100%"
+  // }
+  whiteRectangle:{
+    width: "90%",
+    height:"50%",
+    borderRadius : 20,
+    backgroundColor: "#FFFFFF",
+    zIndex: 0,
+    position: "absolute",
+    alignSelf: "center",
+    marginTop: 180
+  }
+})
 
 export default HomeScreen;
--- components/QuizCategoryCard/index.tsx
+++ components/QuizCategoryCard/index.tsx
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 const QuizCategoryCard = (props:CategoryProps ) => {
     return (
-        <View style = {styles.container}  >
+        <View style = {[styles.container,styles.shadowProp]}  >
             <Text>{props.name}</Text>
             <Button title = "Sci-Fi Quiz" onPress = {() => {props.navigation.navigate('QuizPage')}}></Button>
         </View>
@@ -24,7 +24,17 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 70
+    margin: 10,
+    borderRadius: 20,
+    // shadowOffset: {width: 4, height: 0},
+    // shadowColor: "rgba(0,0,0,0.25)",
+    // shadowOpacity: 100
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
 
