60ca673d04452b846b5750906785eaafdb01c0aa
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-13 12:34:54 +0100

    added Platform specific elevation to create drop shadow for android and used shadow props for ios

--- components/QuizCategoryCard/index.tsx
+++ components/QuizCategoryCard/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
 
-const QuizCategoryCard = (props:CategoryProps ) => {
-    return (
-        <View style = {[styles.container,styles.shadowProp]}  >
-            <Text>{props.name}</Text>
-            <Button title = "Sci-Fi Quiz" onPress = {() => {props.navigation.navigate('QuizPage')}}></Button>
-        </View>
-    )
-}
+const QuizCategoryCard = (props: CategoryProps) => {
+  return (
+    <View style={[styles.container]}>
+      <Text>{props.name}</Text>
+      <Button
+        title="Sci-Fi Quiz"
+        onPress={() => {
+          props.navigation.navigate("QuizPage");
+        }}
+      ></Button>
+    </View>
+  );
+};
 
 type CategoryProps = {
   name: string;
@@ -20,22 +25,24 @@ const styles = StyleSheet.create({
     width: 300,
     height: 150,
     backgroundColor: "#808080",
-    flexDirection: 'column',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
     margin: 10,
     borderRadius: 20,
-    // shadowOffset: {width: 4, height: 0},
-    // shadowColor: "rgba(0,0,0,0.25)",
-    // shadowOpacity: 100
-  },
-  shadowProp: {
-    shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#171717",
+        shadowOffset: { width: -2, height: 4 },
+        shadowOpacity: 0.8,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 6,
+      },
+    }),
   },
 });
 
-export default QuizCategoryCard
+export default QuizCategoryCard;
