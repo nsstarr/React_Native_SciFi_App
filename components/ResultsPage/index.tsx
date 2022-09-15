398248189bc6bb672168312ddd4fd0ac43c2daf8
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-15 10:43:55 +0100

    finished styling the results page

--- components/ResultsPage/index.tsx
+++ components/ResultsPage/index.tsx
@@ -1,52 +1,137 @@
-import React from 'react'
-import { View, Text, Image, Button, ScrollView, StyleSheet } from 'react-native'
-import Header from '../Header'
-import type {NativeStackScreenProps} from "@react-navigation/native-stack"
-
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import Header from "../Header";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+} from "@expo-google-fonts/dev";
+import { LinearGradient } from "expo-linear-gradient";
 
 type StackTypes = {
   Home: undefined;
   QuizPage: undefined;
-  ResultsPage: undefined
-}
+  ResultsPage: undefined;
+};
 
-type Props = NativeStackScreenProps<StackTypes, 'ResultsPage'>
+type Props = NativeStackScreenProps<StackTypes, "ResultsPage">;
 
-const ResultsPage = ({navigation}:Props) => {
-  const score = '4 of 6';
+const ResultsPage = ({ navigation }: Props) => {
+  const score = "4 of 6";
+  const scoreTitle = "Jar Jar Binks";
+  const scoreDescription = "I’m... I’m so sorry, but the results don’t lie";
+
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+  });
+
+  if (fontsLoaded === false) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <Header />
-      <ScrollView contentContainerStyle={styles.scrollView} >
-        <Text>Your Result</Text>
-        <Image source={require('../../assets/Matrix.jpg')} style={{width:300, height: 200}} />
-        <Text>{score}</Text>
-        <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</Text>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Text style={styles.resultsText}>You Scored: {score}%</Text>
+        <Image
+          source={require("../../assets/Matrix.jpg")}
+          style={{ width: 300, height: 200 }}
+        />
+        <Text style={styles.resultsText}>You are: {scoreTitle}</Text>
+        <Text>{scoreDescription}</Text>
         <Text>FINAL LINE</Text>
         <View style={styles.buttonsContainer}>
-          <Button title={'Retake'} onPress={() => {navigation.navigate('QuizPage')}}/>
-          <Button title={'Next Quiz'} onPress={() => {navigation.navigate('Home')}}/>
+          <Pressable
+            style={styles.resultsButton}
+            onPress={() => {
+              navigation.navigate("QuizPage");
+            }}
+          >
+            <LinearGradient
+              colors={["rgba(75,0,130,1)", "rgba(75,0,130,0.64)"]}
+              style={styles.linearGradient}
+            >
+              <Text style={styles.buttonText}>Retake</Text>
+            </LinearGradient>
+          </Pressable>
+          <Pressable
+            style={styles.resultsButton}
+            onPress={() => {
+              navigation.navigate("Home");
+            }}
+          >
+            <LinearGradient
+              colors={["rgba(75,0,130,1)", "rgba(75,0,130,0.64)"]}
+              style={styles.linearGradient}
+            >
+              <Text style={styles.buttonText}>Next Quiz</Text>
+            </LinearGradient>
+          </Pressable>
         </View>
       </ScrollView>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%'
+    height: "100%",
   },
   scrollView: {
-    flexDirection: 'column',
-    alignItems: 'center',
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "75%",
   },
   buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%'
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 3,
+    width: "100%",
+    marginTop: 15,
+  },
+  resultsText: {
+    fontFamily: " Montserrat_400Regular",
+    fontSize: 24,
+    marginBottom: 15,
+    marginTop: 15,
+  },
+  resultsButton: {
+    // backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    borderRadius: 20,
+    height: "auto",
+    width: 130,
+    marginBottom: 60,
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontFamily: "Montserrat_600SemiBold",
+    fontSize: 20,
+    padding: 10,
+    textAlignVertical: "center",
+    textAlign: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  linearGradient: {
+    borderRadius: 20
   }
-})
+});
 
-export default ResultsPage
-
+export default ResultsPage;
