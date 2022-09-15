e0ffbfa476e58b80bed729864eaa8e1fc39aa70d
St0neofFr33dom <christophefreeston@googlemail.com>
2022-09-15 16:11:12 +0100

    Porfile page has a button that reqeusts camera permisiions and it can access the camera. It uses a deprecated function  and it currently can't take pictures

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type Props = NativeStackScreenProps<StackTypes, "Profile">;
+
+type StackTypes = {
+  Profile: undefined
+}
+
+let camera: Camera
 
 
-export default function Profile() {
+
+export default function Profile({route,navigation}:Props) {
+
+  const [startCamera,setStartCamera] = useState(false)
+
+const __startCamera = async ()=>{
+  const {status} = await Camera.requestPermissionsAsync()
+  if(status === 'granted'){
+    // do something
+    setStartCamera(true)
+  }else{
+    Alert.alert("Access denied")
+}}
+
+
+if(startCamera){
+  return(
+    <Camera
+          style={{flex: 1,width:"100%"}}
+          ref={(r) => {
+            camera = r!
+          }}
+        ></Camera>
+  )
+}
+  else 
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+        onPress={__startCamera}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: '#14274e',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 40
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
