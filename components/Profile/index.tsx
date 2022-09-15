bdd6095b50583ccc5a97a9ac851c20e80631f87f
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-09-15 15:17:27 +0100

    Remove no longer needed code

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 
-type StackTypes = {
-  Profile: undefined
-};
-
-type Props = NativeStackScreenProps<StackTypes, "Profile">;
-
-export default function Profile({route, navigation }: Props) {
+export default function Profile() {
   return (
     <View>
       <Text>Profile</Text>
