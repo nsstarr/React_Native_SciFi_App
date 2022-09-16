a3796780d2ded7b639810fc1642c05292d3014e7
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-09-16 17:05:05 +0100

    Create quiz template

---
+++ utilities/QuizTemplate.tsx
@@ -0,0 +1,244 @@
+interface Questions {
+  name: string,
+  image: any,
+  description: string,
+  question: string,
+  answers: {
+      id: string,
+      value: string,
+      label: string,
+      name: string
+      color?:string
+      labelStyle?: any
+      containerStyle?: any
+  }[]
+}
+
+export const quizCardData = {
+  name: "",
+  image: ""
+}
+
+export const questionsData: Array<Questions> = [
+  {
+    name: "image1",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question1", value: "", label: "" },
+      { id: "2", name: "question1", value: "", label: "" },
+      { id: "3", name: "question1", value: "", label: "" },
+      { id: "4", name: "question1", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image2",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question2", value: "", label: "" },
+      { id: "2", name: "question2", value: "", label: "" },
+      { id: "3", name: "question2", value: "", label: "" },
+      { id: "4", name: "question2", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image3",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question3", value: "", label: "" },
+      { id: "2", name: "question3", value: "", label: "" },
+      { id: "3", name: "question3", value: "", label: "" },
+      { id: "4", name: "question3", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image4",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question4", value: "", label: "" },
+      { id: "2", name: "question4", value: "", label: "" },
+      { id: "3", name: "question4", value: "", label: "" },
+      { id: "4", name: "question4", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image5",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question5", value: "", label: "" },
+      { id: "2", name: "question5", value: "", label: "" },
+      { id: "3", name: "question5", value: "", label: "" },
+      { id: "4", name: "question5", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image6",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question6", value: "", label: "" },
+      { id: "2", name: "question6", value: "", label: "" },
+      { id: "3", name: "question6", value: "", label: "" },
+      { id: "4", name: "question6", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image7",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question7", value: "", label: "" },
+      { id: "2", name: "question7", value: "", label: "" },
+      { id: "3", name: "question7", value: "", label: "" },
+      { id: "4", name: "question7", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image8",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question8", value: "", label: "" },
+      { id: "2", name: "question8", value: "", label: "" },
+      { id: "3", name: "question8", value: "", label: "" },
+      { id: "4", name: "question8", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image9",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question9", value: "", label: "" },
+      { id: "2", name: "question9", value: "", label: "" },
+      { id: "3", name: "question9", value: "", label: "" },
+      { id: "4", name: "question9", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image10",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question10", value: "", label: "" },
+      { id: "2", name: "question10", value: "", label: "" },
+      { id: "3", name: "question10", value: "", label: "" },
+      { id: "4", name: "question10", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image11",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question11", value: "", label: "" },
+      { id: "2", name: "question11", value: "", label: "" },
+      { id: "3", name: "question11", value: "", label: "" },
+      { id: "4", name: "question11", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image12",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question12", value: "", label: "" },
+      { id: "2", name: "question12", value: "", label: "" },
+      { id: "3", name: "question12", value: "", label: "" },
+      { id: "4", name: "question12", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image13",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question13", value: "", label: "" },
+      { id: "2", name: "question13", value: "", label: "" },
+      { id: "3", name: "question13", value: "", label: "" },
+      { id: "4", name: "question13", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image14",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question14", value: "", label: "" },
+      { id: "2", name: "question14", value: "", label: "" },
+      { id: "3", name: "question14", value: "", label: "" },
+      { id: "4", name: "question14", value: "", label: "" },
+    ],
+  },
+  {
+    name: "image15",
+    image: require("../assets/"),
+    description: "",
+    question: "",
+    answers: [
+      { id: "1", name: "question15", value: "", label: "" },
+      { id: "2", name: "question15", value: "", label: "" },
+      { id: "3", name: "question15", value: "", label: "" },
+      { id: "4", name: "question15", value: "", label: "" },
+    ],
+  },
+];
+
+export const answerKey = {
+  question1: "",
+  question2: "",
+  question3: "",
+  question4: "",
+  question5: "",
+  question6: "",
+  question7: "",
+  question8: "",
+  question9: "",
+  question10: "",
+  question11: "",
+  question12: "",
+  question13: "",
+  question14: "",
+  question15: ""
+}
+
+export const resultsData = [
+  {
+    name: "",
+    image: require("../assets/"),
+    description: "",
+  },
+  {
+    name: "",
+    image: require("../assets/"),
+    description: "",
+  },
+  {
+    name: "",
+    image: require("../assets/"),
+    description: "",
+  },
+  {
+    name: "",
+    image: require("../assets/"),
+    description: "",
+  },
+];
